f816cbb64302c479b6a70421b4145bd8032837ff
Manoel Ferreira <manoel.ferreira@tusta.com.br>
2023-07-26 15:01:30 -0300

    Added one more configuration to jest

--- jest.config.js
+++ jest.config.js
@@ -6,12 +6,13 @@
 /** @type {import('jest').Config} */
 const config = {
   roots: [
-    "<rootDir>"
+    "<rootDir>/src"
   ],
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
